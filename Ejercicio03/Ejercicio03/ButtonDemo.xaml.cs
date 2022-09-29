35e361765c6abcd9597b85b8a4e1e28fd15d6ba7
tecsupaponte <112447494+tecsupaponte@users.noreply.github.com>
2022-09-28 19:51:42 -0500

    Add project files.

---
+++ Ejercicio03/Ejercicio03/ButtonDemo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonDemo : ContentPage
+    {
+        public ButtonDemo()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await Label.RelRotateTo(360, 1000);
+        }
+    }
+}
